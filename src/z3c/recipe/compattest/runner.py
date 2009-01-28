8c67479d31dfa8ac5bbab0b0232cc40e458b9a5c
Sylvain Viollon <thefunny@gmail.com>
2009-01-28 12:07:47 +0000

    Add an usage.

--- src/z3c/recipe/compattest/runner.py
+++ src/z3c/recipe/compattest/runner.py
@@ -2,8 +2,20 @@ import subprocess
 import sys
 import os.path
 
+def usage():
+    print """
+usage: %s [OPTIONS]
+
+All given options will be passed to each test runner. To know which
+options you can use, refer to the test runner documentation.
+""" % sys.argv[0]
+
 def main(*scripts):
     failed = dict()
+    argv = sys.argv[1:]
+    if '-h' in argv or '--help' in argv:
+        usage()
+        return
 
     for script in scripts:
         print "Running %s" % os.path.basename(script)
