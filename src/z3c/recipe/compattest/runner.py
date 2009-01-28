16b544b32a412fa3968a7016fc6cb9b34c85a95c
Sylvain Viollon <thefunny@gmail.com>
2009-01-28 11:55:56 +0000

    Improve the runner.

--- src/z3c/recipe/compattest/runner.py
+++ src/z3c/recipe/compattest/runner.py
@@ -1,6 +1,23 @@
-import os
-
+import subprocess
+import sys
+import os.path
 
 def main(*scripts):
+    failed = dict()
+
     for script in scripts:
-        os.system(script)
+        print "Running %s" % os.path.basename(script)
+        p = subprocess.Popen(
+            [script, '--exit-with-status'] + sys.argv[1:],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            close_fds=True)
+        p.wait()
+        if p.returncode:
+            failed[script] = p.stdout.read()
+            print "Failed with:"
+            print failed[script]
+
+    print "%d failures.%s" % (
+        len(failed), '\n- '.join(failed.keys()))
+
