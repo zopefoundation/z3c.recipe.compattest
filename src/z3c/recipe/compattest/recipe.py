f4da35b772379f4151318a6c2ba56ababb79b6af
Christian Theune <ct@gocept.com>
2009-01-29 10:24:21 +0000

    Always enable all extras for packages under test.

--- src/z3c/recipe/compattest/recipe.py
+++ src/z3c/recipe/compattest/recipe.py
@@ -95,7 +95,9 @@ class Recipe(object):
         installed = []
         for package in self.wanted_packages:
             try:
-                needs_test = self._needs_test_dependencies(package)
+                ws = self._working_set(package)
+                package_ = ws.find(pkg_resources.Requirement.parse(package))
+                extras = '[' + ','.join(package_.extras) + ']'
             except zc.buildout.easy_install.MissingDistribution, e:
                 if e.data[0].project_name == package and not self.use_svn:
                     # This package has a project in subversion but no
@@ -103,11 +105,7 @@ class Recipe(object):
                     print "No release found for %s. Ignoring." % package
                     continue
                 raise
-            if needs_test:
-                extra = ' [test]'
-            else:
-                extra = ''
-            options = dict(eggs=package + extra)
+            options = dict(eggs=package + extras)
             recipe = zc.recipe.testrunner.TestRunner(
                 self.buildout, '%s-%s' % (self.name, package), options)
             installed.extend(recipe.install())
@@ -143,11 +141,6 @@ class Recipe(object):
             if include:
                 projects.append(project)
         return projects
-
-    def _needs_test_dependencies(self, package):
-        ws = self._working_set(package)
-        package = ws.find(pkg_resources.Requirement.parse(package))
-        return 'test' in package.extras
 
     def _working_set(self, package):
         eggs = zc.recipe.egg.Egg(self.buildout, self.name, dict(eggs=package))
